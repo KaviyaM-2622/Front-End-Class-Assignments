5787a4cb28a607768bb2cc88de94dd65d44d9e06
KaviyaM-2622 <mkaviya2622@gmail.com>
2025-11-11 21:16:02 +0530

    created

---
+++ Day 26/Task5.js
@@ -0,0 +1,8 @@
+let name = "Kaviya";
+let age = 24;
+let city = "Chennai";
+
+console.log(`User Details:
+Name: ${name}
+Age: ${age}
+City: ${city}`);
---
+++ Day 26/Task6.js
@@ -0,0 +1,6 @@
+function showDetails(...args) {
+  console.log("Number of arguments:", args.length);
+  console.log("Values:", args);
+}
+showDetails("Kaviya", 24, "Student", "India");
+showDetails(10, 20, 30);
