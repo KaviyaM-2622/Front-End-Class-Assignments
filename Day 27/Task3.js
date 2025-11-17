41f908a199c0da94dad0c11268b2a829eefaa996
KaviyaM-2622 <mkaviya2622@gmail.com>
2025-11-17 12:34:47 +0530

    created

---
+++ Day 27/Task3.js
@@ -0,0 +1,18 @@
+async function fetchGitHubData() {
+  try {
+    const response = await fetch("https://api.github.com/users/KaviyaM-2622");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data. Status: " + response.status);
+    }
+
+    const data = await response.json();
+    console.log("Login:", data.login);
+    console.log("Public Repos:", data.public_repos);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+fetchGitHubData();
+
